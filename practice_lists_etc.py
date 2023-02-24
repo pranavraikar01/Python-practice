247d691ffce40a2facdd35bfc5c7bac3b7090757
PRANAV RAIKAR <pranavraikar01@gmail.com>
2023-02-24 12:32:39 +0530

    added more info

--- practice_lists_etc.py
+++ practice_lists_etc.py
@@ -1,4 +1,4 @@
-#-----lists------
+# #-----lists------
 # food=["Pizza","burger","shwarma"]
 # # print(food)
 # # print(food[0])
@@ -12,3 +12,22 @@
 # for i in food:   
 #     print(i)
 # food.clear()                      #deletes the list
+
+
+# #-----2Dlists------
+# drinks=["cofee","soda","tea"]
+# dinner=["chapati","rice","daal"]
+# lunch=["bhaji","bhakri","fish"]
+# food=[drinks,dinner,lunch]
+# food[0][0]="solkadhi"
+# print(food)
+
+
+# #-----tupples----    it is collection of data which is ordered and is unchangeble and unlike list we can store duplicate data in tupple
+# student=("Pranav",20,"male","Pranav")
+# print(student.count("Pranav"))          #counts how many times pranav is repeated
+# print(student.index("Pranav"))          #gives the index position of pranav if repeated then it gives index of word which comes first
+# for i in student:
+#     print(i)
+# if "Pranav" in student:                 #checks whether pranav is there in the tupple or not
+#     print("hello pranav")
