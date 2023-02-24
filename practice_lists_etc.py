7b04116426cf15d05d822a136eea6773e89aa3f9
PRANAV RAIKAR <pranavraikar01@gmail.com>
2023-02-24 17:00:35 +0530

    added new data

--- practice_lists_etc.py
+++ practice_lists_etc.py
@@ -31,3 +31,37 @@
 #     print(i)
 # if "Pranav" in student:                 #checks whether pranav is there in the tupple or not
 #     print("hello pranav")
+
+
+##-----set----   it is a collection which is unordered and,unindexed.No duplicate values it is faster than a list
+# utensils={"knife","spoon","fork"}
+# dishes={"bowl","plate","cup"}
+
+# utensils.add("napkin")                   #adds new value in the set
+# utensils.remove("fork")                  #removes value from the set
+# utensils.update(dishes)                  #adds one set into another
+# dinner_table=utensils.union(dishes)       #adds one set into other   if we do intersection then what is common between both that is written 
+# print(utensils.difference(dishes))          #prints what utensils has that dishes doesn't
+# for i in utensils:              
+#     print(i)                    #it prints in an unordered fashion
+# for j in dinner_table:
+#     print(j)
+
+
+
+# #-----dictionary--- a changeble unordered caollection of uniwue key:value pairs it is fast becoause uses hashing 
+# Capitals={'USA':'Washington dc',
+#           'India':'new Delhi',
+#           'China':'Bejing',
+#           'Germany':'Berlin'}
+# print(Capitals.get('India'))          #we have to give input as key then here in code then it will give the value of that keya as output
+# print(Capitals.get('Russia'))          #since there is no key as Russia it gives None as output
+# print(Capitals.keys())                  #prints all the keys
+# print(Capitals.values())                #prints all the values
+# Capitals.update({'USA':'CALIFORNIA'})        #changes the value of key
+# Capitals.update({'Spain':'Paris'})            #adds new key value pair
+# print(Capitals.items())                 #prints both keys and values
+# Capitals.pop('China')                   #deletes some key value pair using key from dictioanry
+# for key,value in Capitals.items():      #also prints both keys and values
+#     print(key,value)
+# Capitals.clear()                        #clears the dictionary
