528666578ee1c9e4ac06b080f01332e7193cbffd
PRANAV RAIKAR <pranavraikar01@gmail.com>
2023-02-24 06:27:16 +0530

    file eddited

---
+++ practice_lists_etc.py
@@ -0,0 +1,14 @@
+#-----lists------
+# food=["Pizza","burger","shwarma"]
+# # print(food)
+# # print(food[0])
+# # food[1]="vadapav"      #we can change the elements of list
+# # print(food[1])
+# food.append("icecream")
+# food.remove("Pizza")
+# food.pop()                      #removes last element of list
+# food.insert(1,"cake")            #inserts at certain position of list
+# food.sort()                      #sorts the list
+# for i in food:   
+#     print(i)
+# food.clear()                      #deletes the list
