b542808609eabb4799fc480ce24f61285f521c43
PRANAV RAIKAR <pranavraikar01@gmail.com>
2023-02-25 22:00:25 +0530

    added new file

---
+++ File_handling.py
@@ -0,0 +1,11 @@
+# #----file detection---
+# import os
+# path="C:\\Users\\Pranav\\Desktop\\pranav.txt"    #NOTE:Even if there are single back slash in original path edit it and add double where they are,also the file must exist
+# if os.path.exists(path):
+#     print("the location exists")
+#     if os.path.isfile(path):
+#         print("It is a file")
+#     elif os.path.isdir(path):
+#         print("It is a directory")
+# else:
+#     print("The location doesnt exist")
