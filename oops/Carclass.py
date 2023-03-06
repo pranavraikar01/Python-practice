4c1aebf9a31bd50fb61a36b7282d6ee3d7c37a40
PRANAV RAIKAR <pranavraikar01@gmail.com>
2023-03-06 05:36:40 +0530

    added new code

--- oops/Carclass.py
+++ oops/Carclass.py
@@ -1,10 +1,14 @@
 class Car:
+
+    wheels=4        #class variable
+    #a class variable is a variable inside the class but outside the constructor
     #__init__ is a constructor in python
     def __init__(self,make,model,year,colour):        #while calliong a function dont need to pass self but while defining function it is necessary
-        self.make=make                               
-        self.model=model
-        self.year=year
-        self.colour=colour
+        #instance variable is a variable which is inside the constructor
+        self.make=make               #instance variable                          
+        self.model=model             #instance variable 
+        self.year=year              #instance variable
+        self.colour=colour           #instance variable
         
     def drive(self):
         print("the " +self.model +" is driving")    #in simple words self acts as a link between object in real function and its class which is created at different place
--- oops/oop1.py
+++ oops/oop1.py
@@ -1,5 +1,6 @@
 from Carclass import Car
 c1=Car("German","Honda","2021","Black")
+c2=Car("American","Tesla","2022","white")
 print(c1.make)
 print(c1.model)
 print(c1.year)
@@ -7,3 +8,5 @@ print(c1.colour)
 
 c1.drive()
 c1.stop()
+print(c1.wheels)
+print(c2.wheels)
